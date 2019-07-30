7469fa9053b75dc45c132a77b26120e09f5a2bb1
GlennJenkins <glenn.jenkins@digitickets.co.uk>
2019-07-30 09:51:21 +0100

    Another update from Totally.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,11 @@
 ## 0.1.1
 
   - Downgrade Guzzle from to 1.6.x to 1.5.2
+
+## 0.1.2
+
+  - Downgrades PHP to 7.0
+  
+## 0.1.3
+  
+  - Removes Guzzle for tests
--- tests/Message/CompleteRedirectPurchaseResponseFailureTest.php
+++ tests/Message/CompleteRedirectPurchaseResponseFailureTest.php
@@ -2,14 +2,9 @@
 
 namespace Omnipay\GlobalPayments\Test\Message;
 
-use Omnipay\Common\Message\RequestInterface;
-use Omnipay\GlobalPayments\Gateway;
-
 use Omnipay\GlobalPayments\Message\CompleteRedirectPurchaseRequest;
-use Omnipay\GlobalPayments\Message\CompleteRedirectPurchaseResponse;
-
+use function Omnipay\PaymentSense\Test\Gateway\getMockHttpRequest;
 use Omnipay\Tests\TestCase;
-use ReflectionObject;
 use Symfony\Component\HttpFoundation\Request;
 
 class CompleteRedirectPurchaseResponseFailureTest extends TestCase
@@ -45,42 +40,17 @@ class CompleteRedirectPurchaseResponseFailureTest extends TestCase
      */
     public function getRequestFromFile(string $fixture)
     {
-        $requestMock = $this->getMockHttpRequest($fixture);
-        parse_str($requestMock->getBody()->getContents(), $data);
+        $requestMock = getMockHttpRequest($fixture);
+        parse_str($requestMock['body'], $data);
 
         $request = new CompleteRedirectPurchaseRequest(
             $this->getHttpClient(),
             new Request([], $data, [], [], [], [],
-                $requestMock->getBody()->getContents())
+                $requestMock['body'])
         );
 
         $request->setSharedSecret('secret');
 
         return $request;
-
-    }
-
-    /**
-     * Get a mock response for a client by mock file name
-     *
-     * @param string $path Relative path to the mock response file
-     *
-     * @return ResponseInterface
-     */
-    public function getMockHttpRequest($path)
-    {
-        if ($path instanceof RequestInterface) {
-            return $path;
-        }
-
-        $ref = new ReflectionObject($this);
-        $dir = dirname($ref->getFileName());
-
-        // if mock file doesn't exist, check parent directory
-        if (!file_exists($dir . '/Mock/' . $path) && file_exists($dir . '/../Mock/' . $path)) {
-            return \GuzzleHttp\Psr7\parse_request(file_get_contents($dir . '/../Mock/' . $path));
-        }
-
-        return \GuzzleHttp\Psr7\parse_request(file_get_contents($dir . '/Mock/' . $path));
     }
 }
